9687b75cf94a8578ac98f7a9a2dd27dc07dca0c4
thegabriele97 <gabriele.lagreca@gmail.com>
2018-04-26 16:12:31 +0200

    new version

--- Ex 4/ES1.asm
+++ Ex 4/ES1.asm
@@ -1,12 +1,12 @@
-INT_W_STR EQU 9H
+INT_W_STR EQU 0x9
 
         .model small
         .stack
         .data
         
-COE_A   DW 2
-COE_B   DW 6
-COE_C   DW 4
+COE_A   DW 35
+COE_B   DW -6
+COE_C   DW -42
 
 MSG_1   DB 'NO REAL SOLUTION', 024H
 MSG_2   DB 'HAS ONE OR TWO REAL SOLUTION', 024H        
@@ -14,26 +14,35 @@ MSG_2   DB 'HAS ONE OR TWO REAL SOLUTION', 024H
         .code
         .startup
         
-        MOV AX, COE_B       ;COMPUTING 
-        MOV BX, AX          ;SQUARE OF COE_B
-        MUL BX
-        MOV CX, AX          ;SAVING IT IN CX
+        MOV AX, COE_A
+        IMUL AX             ;COE_A*COE_A
+        JO OF_ERR
         
-        MOV AX, COE_A       ;COMPUTING 
-        MOV BX, COE_C       ;COE_A*COE_B
-        MUL BX
+        MOV BX, AX          ;STORING COE_A*COE_A
         
-        SHL AX, 2           ;MUL IT BY 4
-        SUB CX, AX          ;CX -= AX
-        JS NO_SOL           ;IF IS <0 ->NO REAL SOLUTION
+        MOV AX, COE_B
+        IMUL COE_C          ;COE_B*COE_C
+        JO OF_ERR
         
+        SAL AX, 1           ;MUL BY 2
+        JO OF_ERR           ;CHECK FOR OF
+        SAL AX, 1           ;MUL BY 2
+        JO OF_ERR           ;CHECK FOR OF
+        
+        SUB BX, AX
+        JS NO_SOL
+         
         MOV DX, OFFSET MSG_2
-        JMP END_
+        JMP PRINT
         
-NO_SOL: MOV DX, OFFSET MSG_1                        
+NO_SOL: MOV DX, OFFSET MSG_1        
         
-END_:   MOV AH, INT_W_STR   ;PRINT MSG
-        INT 21H
+PRINT:  MOV AH, INT_W_STR
+        INT 0x21
+        JMP KILL      
         
+OF_ERR: INT 0H
+
+KILL:        
         .exit
         END
